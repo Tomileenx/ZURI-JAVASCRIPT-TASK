f54d80c28ce98851719f470a142d6ccf5230db04
Tomileenx <olarewajuoluwatomi@gmail.com>
2022-06-23 03:49:31 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const myName = "Oluwatomi";
+const myHeight = "5ft 10"
+const myCountry = "Nigeria";
+
+document.querySelector(".p1").innerHTML = "Name: "+ myName;
+document.querySelector(".p2").innerHTML = "Height: " + myHeight;
+document.querySelector(".p3").innerHTML = "Country: " + myCountry;
